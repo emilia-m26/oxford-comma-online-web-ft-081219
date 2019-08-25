05abf8916e932dcd8ea8561cb705975413353c37
Emilia ☆ <emilia.mena26@gmail.com>
2019-08-25 21:43:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,15 @@
 def oxford_comma(array)
-
-
+  if array.length == 2 
+    array.join("and")
+    
+  elsif 
+    array.length >= 3
+    array.pop
+    array << "and"
+  
+  else
+    
+  end
 end
 
 
